d90dca8e5fd8e67269baf340e7631b5de218a6b8
Yoonjin Im <skewondr@ajou.ac.kr>
2020-03-05 02:12:51 +0900

    version 1.0

--- comments/views.py
+++ comments/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.utils import timezone
+from .models import Post
+from .forms import PostForm
 
 def index(request):
     return HttpResponse("Hello, world. You're at the test page.")
@@ -20,4 +23,22 @@ def result(request):
     return
 
 def post_list(request):
-     return render(request, 'comments/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'comments/post_list.html', {'posts':posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'comments/post_detail.html', {'post': post})
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
